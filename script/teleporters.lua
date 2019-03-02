f809fd84da9501644e7776813b6fa402e2e9960f
Klonan <11986037+Klonan@users.noreply.github.com>
2019-03-02 14:40:21 +0100

    Two-way linkage between teleporter and their map tags

--- script/teleporters.lua
+++ script/teleporters.lua
@@ -5,10 +5,11 @@ local data =
   networks = {},
   rename_frames = {},
   button_actions = {},
-  map = {},
+  teleporter_map = {},
   teleporter_frames = {},
   player_linked_teleporter = {},
-  to_be_removed = {}
+  to_be_removed = {},
+  tag_map = {}
 }
 
 local print = function(string)
@@ -66,6 +67,14 @@ local make_rename_frame = function(player, caption)
   local cancel = frame.add{type = "sprite-button", sprite = "utility/set_bar_slot", style = "slot_button"}
   util.register_gui(data.button_actions, cancel, {type = "cancel_rename"})
 
+end
+
+local get_force_color = function(force)
+  local player = force.connected_players[1]
+  if player and player.valid then
+    return player.chat_color
+  end
+  return {r = 1, b = 1, g = 1}
 end
 
 local unlink_teleporter = function(player)
@@ -169,6 +178,70 @@ local check_player_linked_teleporter = function(player)
   end
 end
 
+local clear_teleporter_data = function(teleporter_data)
+  local flying_text = teleporter_data.flying_text
+  if flying_text and flying_text.valid then
+    flying_text.destroy()
+  end
+  local map_tag = teleporter_data.tag
+  if map_tag and map_tag.valid then
+    data.tag_map[map_tag.tag_number] = nil
+    map_tag.destroy()
+  end
+end
+
+local resync_teleporter = function(name, teleporter_data)
+  local teleporter = teleporter_data.teleporter
+  if not (teleporter and teleporter.valid) then
+    return
+  end
+  local force = teleporter.force
+  local surface = teleporter.surface
+  local color = get_force_color(force)
+
+  clear_teleporter_data(teleporter_data)
+
+  local flying_text = teleporter.surface.create_entity
+  {
+    name = "teleporter-flying-text",
+    text = name,
+    position = {teleporter.position.x, teleporter.position.y - 2},
+    force = force,
+    color = color
+  }
+  flying_text.active = false
+  teleporter_data.flying_text = flying_text
+
+  data.adding_tag = true
+  local map_tag = force.add_chart_tag(surface,
+  {
+    icon = {type = "item", name = teleporter_name},
+    position = teleporter.position,
+    text = name
+  })
+  data.adding_tag = false
+
+  if map_tag then
+    teleporter_data.tag = map_tag
+    data.tag_map[map_tag.tag_number] = teleporter_data
+  end
+
+end
+
+local is_name_available = function(force, name)
+  local network = data.networks[force.name]
+  return not network[name]
+end
+
+local rename_teleporter = function(force, old_name, new_name)
+  local network = data.networks[force.name]
+  local teleporter_data = network[old_name]
+  network[new_name] = teleporter_data
+  network[old_name] = nil
+  resync_teleporter(new_name, teleporter_data)
+  refresh_teleporter_frames()
+end
+
 local gui_actions =
 {
   rename_button = function(event, param)
@@ -184,22 +257,14 @@ local gui_actions =
     if not (flying_text and flying_text.valid) then return end
     local player = game.players[event.player_index]
     if not (player and player.valid) then return end
-    local key = flying_text.text
-    local network = data.networks[player.force.name]
-    local info = network[key]
-    local new_key = param.textfield.text
-    if network[new_key] and network[new_key] ~= info then
-      player.print({"name-already-taken"})
-      return
-    end
-    if new_key ~= key then
-      network[new_key] = info
-      network[key] = nil
-      param.flying_text.text = new_key
-      if param.tag and param.tag.valid then
-        param.tag.text = new_key
-        param.tag.last_user = player
+    local old_name = flying_text.text
+    local new_name = param.textfield.text
+    if new_name ~= old_name then
+      if not is_name_available(player.force, new_name) then
+        player.print({"name-already-taken"})
+        return
       end
+      rename_teleporter(player.force, old_name, new_name)
     end
     close_gui(get_rename_frame(player))
     check_player_linked_teleporter(player)
@@ -222,102 +287,44 @@ local gui_actions =
   end
 }
 
-local get_force_color = function(force)
-  local player = force.connected_players[1]
-  if player and player.valid then
-    return player.chat_color
-  end
-  return {r = 1, b = 1, g = 1}
-end
-
-local on_robot_built_entity = function(event)
-  local entity = event.created_entity
-  if not (entity and entity.valid) then return end
-  if entity.name ~= teleporter_name then return end
-  local surface = entity.surface
-  local force = entity.force
-  local caption = "Teleporter ".. entity.unit_number
-  local text = surface.create_entity
-  {
-    name = "teleporter-flying-text",
-    text = caption,
-    position = {entity.position.x, entity.position.y - 2},
-    force = force,
-    color = get_force_color(force)
-  }
-  text.active = false
-
-  local tag = force.add_chart_tag(surface,
-  {
-    icon = { type = "item", name = teleporter_name},
-    position = entity.position,
-    text = caption
-  }
-  )
-
-  data.networks[force.name] = data.networks[force.name] or {}
-  local network = data.networks[force.name]
-  local teleport_data = {teleporter = entity, flying_text = text, tag = tag}
-  network[caption] = teleport_data
-  data.map[entity.unit_number] = teleport_data
-
-  refresh_teleporter_frames()
+local get_network = function(force)
+  local name = force.name
+  local network = data.networks[name]
+  if network then return network end
+  data.networks[name] = {}
+  return data.networks[name]
 end
 
 local on_built_entity = function(event)
   local entity = event.created_entity
   if not (entity and entity.valid) then return end
   if entity.name ~= teleporter_name then return end
-  local player = game.players[event.player_index]
   local surface = entity.surface
   local force = entity.force
-  local caption = "Teleporter "..entity.unit_number
-  local text = surface.create_entity
-  {
-    name = "teleporter-flying-text",
-    text = caption,
-    position = {entity.position.x, entity.position.y - 2},
-    force = force,
-    color = player.chat_color
-  }
-  text.active = false
-
-  local tag = force.add_chart_tag(surface,
-  {
-    icon = { type = "item", name = teleporter_name},
-    position = entity.position,
-    text = caption,
-    last_user = player
-  }
-  )
-
-  data.networks[force.name] = data.networks[force.name] or {}
-  local network = data.networks[force.name]
-  local teleport_data = {teleporter = entity, flying_text = text, tag = tag}
-  network[caption] = teleport_data
-  data.map[entity.unit_number] = teleport_data
-
-  make_rename_frame(player, caption)
+  local name = "Teleporter ".. entity.unit_number
+  local network = get_network(force)
+  local teleporter_data = {teleporter = entity, flying_text = text, tag = tag}
+  network[name] = teleporter_data
+  data.teleporter_map[entity.unit_number] = teleporter_data
+  resync_teleporter(name, teleporter_data)
   refresh_teleporter_frames()
+  if event.player_index then
+    make_rename_frame(game.get_player(event.player_index), name)
+  end
 end
 
 local on_teleporter_removed = function(entity)
   if not (entity and entity.valid) then return end
   if entity.name ~= teleporter_name then return end
   local force = entity.force
-  local param = data.map[entity.unit_number]
-  if not param then return end
-  local caption = param.flying_text.text
-  local network = data.networks[force.name]
+  local teleporter_data = data.teleporter_map[entity.unit_number]
+  if not teleporter_data then return end
+  local caption = teleporter_data.flying_text.text
+  local network = get_network(force)
   network[caption] = nil
-  param.flying_text.destroy()
+  clear_teleporter_data(teleporter_data)
+  data.teleporter_map[entity.unit_number] = nil
 
-  local tag = param.tag
-  if tag and tag.valid then
-    tag.destroy()
-  end
-
-  data.map[entity.unit_number] = nil
   data.to_be_removed[entity.unit_number] = true
   refresh_teleporter_frames()
   data.to_be_removed[entity.unit_number] = nil
@@ -330,7 +337,7 @@ local teleporter_triggered = function(entity)
   local force = entity.force
   local surface = entity.surface
   local position = entity.position
-  local param = data.map[entity.unit_number]
+  local param = data.teleporter_map[entity.unit_number]
   local new_teleporter = surface.create_entity
   {
     name = teleporter_name,
@@ -339,8 +346,8 @@ local teleporter_triggered = function(entity)
     create_build_effect_smoke = false
   }
   param.teleporter = new_teleporter
-  data.map[new_teleporter.unit_number] = param
-  data.map[entity.unit_number] = nil
+  data.teleporter_map[new_teleporter.unit_number] = param
+  data.teleporter_map[entity.unit_number] = nil
   local character = surface.find_entities_filtered{type = "player", area = {{position.x - 2, position.y - 2}, {position.x + 2, position.y + 2}}, force = force}[1]
   if not character then return end
   local player = character.player
@@ -386,8 +393,6 @@ end
 
 local on_gui_closed = function(event)
   --print("CLOSED "..event.tick)
-  local element = event.element
-  if not (element and element.valid) then return end
 
   local player = game.get_player(event.player_index)
 
@@ -398,7 +403,12 @@ local on_gui_closed = function(event)
     return
   end
 
-  unlink_teleporter(player)
+  local teleporter_frame = get_teleporter_frame(player)
+  if teleporter_frame then
+    close_gui(teleporter_frame)
+    unlink_teleporter(player)
+    return
+  end
 
 end
 
@@ -408,10 +418,99 @@ local on_player_removed = function(event)
   unlink_teleporter(player)
 end
 
+local resync_all_teleporters = function()
+  for force, network in pairs (data.networks) do
+    for name, teleporter_data in pairs (network) do
+      resync_teleporter(name, teleporter_data)
+    end
+  end
+end
+
+--[[on_chart_tag_modified
+
+Called when a chart tag is modified by a player.
+
+Contains
+tag :: LuaCustomChartTag
+player_index :: uint (optional)
+force :: LuaForce
+old_text :: string
+old_icon :: SignalID
+old_player_index :: uint (optional)]]
+
+local on_chart_tag_modified = function(event)
+  local force = event.force
+  local tag = event.tag
+  if not (force and force.valid and tag and tag.valid) then return end
+  local teleporter_data = data.tag_map[tag.tag_number]
+  if not teleporter_data then
+    --Nothing to do with us...
+    return
+  end
+  local player = event.player_index and game.get_player(event.player_index)
+
+  local old_name = event.old_text
+  local new_name = tag.text
+  if tag.icon and tag.icon.name ~= teleporter_name then
+    --They're trying to modify the icon! Straight to JAIL!
+    if player and player.valid then player.print({"cant-change-icon"}) end
+    tag.icon = {type = "item", name = teleporter_name}
+  end
+  if new_name == old_name then
+    return
+  end
+  if new_name == "" or not is_name_available(force, new_name) then
+    if player and player.valid then
+      player.print({"name-already-taken"})
+    end
+    tag.text = old_name
+    return
+  end
+  rename_teleporter(force, old_name, new_name)
+end
+
+--[[on_chart_tag_removed
+
+Called just before a chart tag is deleted.
+
+Contains
+tag :: LuaCustomChartTag
+force :: LuaForce
+player_index :: uint (optional)]]
+
+local on_chart_tag_removed = function(event)
+  local force = event.force
+  local tag = event.tag
+  if not (force and force.valid and tag and tag.valid) then return end
+  local teleporter_data = data.tag_map[tag.tag_number]
+  if not teleporter_data then
+    --Nothing to do with us...
+    return
+  end
+  local name = tag.text
+  resync_teleporter(name, teleporter_data)
+end
+
+local on_chart_tag_added = function(event)
+  if data.adding_tag then return end
+  local tag = event.tag
+  if not (tag and tag.valid) then
+    return
+  end
+  local icon = tag.icon
+  if icon.type == "item" and icon.name == teleporter_name then
+    --Trying to add a fake teleporter tag! JAIL!
+    local player = event.player_index and game.get_player(event.player_index)
+    if player and player.valid then player.print({"cant-add-tag"}) end
+    tag.destroy()
+    return
+  end
+end
+
 local events =
 {
   [defines.events.on_built_entity] = on_built_entity,
-  [defines.events.on_robot_built_entity] = on_robot_built_entity,
+  [defines.events.on_robot_built_entity] = on_built_entity,
   [defines.events.on_gui_click] = on_gui_click,
   [defines.events.on_entity_died] = on_entity_died,
   [defines.events.on_player_mined_entity] = on_player_mined_entity,
@@ -419,7 +518,10 @@ local events =
   [defines.events.on_gui_closed] = on_gui_closed,
   [defines.events.on_player_died] = on_player_removed,
   [defines.events.on_player_left_game] = on_player_removed,
-  [defines.events.on_player_changed_force] = on_player_removed
+  [defines.events.on_player_changed_force] = on_player_removed,
+  [defines.events.on_chart_tag_modified] = on_chart_tag_modified,
+  [defines.events.on_chart_tag_removed] = on_chart_tag_removed,
+  [defines.events.on_chart_tag_added] = on_chart_tag_added,
 }
 
 local teleporters = {}
@@ -443,6 +545,9 @@ teleporters.on_configuration_changed = function()
   data.player_linked_teleporter = data.player_linked_teleporter or {}
   data.rename_frames = data.rename_frames or data.frames or {}
   data.to_be_removed = data.to_be_removed or {}
+  data.teleporter_map = data.teleporter_map or data.map or {}
+  data.tag_map = data.tag_map or {}
+  resync_all_teleporters()
 end
 
 return teleporters
